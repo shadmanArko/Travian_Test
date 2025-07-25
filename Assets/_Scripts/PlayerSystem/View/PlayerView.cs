1a56dd71ec5940d4fa1ba3b394165ca3387898b6
Shadman Arko <35429182+shadmanArko@users.noreply.github.com>
2025-07-25 11:30:23 +0200

    Add playerView

---
+++ Assets/_Scripts/PlayerSystem/View/PlayerView.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+namespace _Scripts.PlayerSystem.View
+{
+    public class PlayerView : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private Collider2D col;
+
+        public event Action OnJumpInput;
+        public event Action<Collider2D> OnCollisionDetected;
+
+        private void Awake()
+        {
+            if (rb == null) rb = GetComponent<Rigidbody2D>();
+            if (col == null) col = GetComponent<Collider2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OnJumpInput?.Invoke();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            OnCollisionDetected?.Invoke(other.collider);
+        }
+        
+        public void SetPosition(Vector3 position)
+        {
+           transform.position = position;
+        }
+        
+        public void ApplyJumpForce(float force)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, force);
+        }
+        
+        public void SetGravity(float gravity)
+        {
+            rb.gravityScale = gravity;
+        }
+        
+        public Vector3 GetPosition()
+        {
+            return transform.position;
+        }
+
+        public bool IsGroundedCheck()
+        {
+            return rb.linearVelocity.y <= 0.1f && transform.position.y <= 0.1f;
+        }
+    }
+}
